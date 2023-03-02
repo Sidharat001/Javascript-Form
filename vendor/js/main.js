65a983047e9596533aca02f62438ada3bde7d88e
sidharat001 <dr.sidharat@gmail.com>
2023-03-02 11:27:36 +0530

    this is form validation in js

--- vendor/js/main.js
+++ vendor/js/main.js
@@ -3,22 +3,26 @@ let btn = document.getElementById('delete').onclick = function(){
     console.log(demo);
 }
 
-function addUser(){
+function addUser(e){
+    // e.preventdefault();
     var name = document.getElementById('AddUsername').value;
     var lastName = document.getElementById('AddUserlastName').value;
     var email = document.getElementById('AddUserEmail').value;
     var phone = document.getElementById('AddUserNumber').value;
     var address = document.getElementById('AddUserAddress').value;
 
+    //  Name Validation Apply
     if(name.length < 2 || name.length > 10){
         var NamealertBox = document.getElementById('NameError');
         NamealertBox.innerHTML = "Name Must Be More Than 2 and Lessthan 10 character";
         NamealertBox.style.fontSize = "12px";
+        console.log(name);
     }else{
         var NamealertBox = document.getElementById('NameError');
         NamealertBox.innerHTML = "";
     }
 
+    //  LastName Validation Apply
     if(lastName.length < 2 || lastName.length > 10){
         var __lastNamealertBox = document.getElementById('lastNameError');
         __lastNamealertBox.innerHTML = "Last Name Must Be More Than 2 and Lessthan 10 character";
@@ -28,7 +32,7 @@ function addUser(){
         __lastNamealertBox.innerHTML = "";
     }
 
-    //How to email validation apply in js
+    //  email validation apply
     if(email.indexOf("@") == -1 || email.indexOf(".") == -1){
         var EmailalertBox = document.getElementById('EmailError');
         EmailalertBox.innerHTML = "Email Must Be Valid";
@@ -36,6 +40,26 @@ function addUser(){
     }else{
         var EmailalertBox = document.getElementById('EmailError');
         EmailalertBox.innerHTML = "";
+    }
+
+    // apply phone validation in
+    if(phone.length!= 10){
+        var PhonealertBox = document.getElementById('PhoneError');
+        PhonealertBox.innerHTML = "Phone Number Must Be 10 digit";
+        PhonealertBox.style.fontSize = "12px";
+    }else{
+        var PhonealertBox = document.getElementById('PhoneError');
+        PhonealertBox.innerHTML = "";
+    }
+    
+    //address validation
+    if (address.length < 5 || address.length > 50) {
+        var AddressalertBox = document.getElementById('AddressError');
+        AddressalertBox.innerHTML = "Address Must Be More Than 5 and Lessthan 50 character";
+        AddressalertBox.style.fontSize = "12px";
+    } else {
+        var AddressalertBox = document.getElementById('AddressError');
+        AddressalertBox.innerHTML = "";
     }
     
     
@@ -47,4 +71,19 @@ function addUser(){
         address: address
     }
     console.log(user);
+
+    // Store User Object In Databse by Jquery 
+    // $.ajax({
+    //     type: "POST",
+    //     url: "http://localhost:3000/users",
+    //     data: JSON.stringify(user),
+    //     contentType: "application/json; charset=utf-8",
+    //     dataType: "json",
+    //     success: function (data) {
+    //         console.log(data);
+    //     },
+    //     error: function (err) {
+    //         console.log(err);
+    //     }
+    // });
 }
